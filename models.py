fd727c02a306e1d531a3cdf2733ddbde838a5d63
woodsgao <woodsgao@outlook.com>
2019-11-01 13:18:12 +0800

    init weight and bias

--- models.py
+++ models.py
@@ -65,6 +65,15 @@ class DeepLabV3Plus(nn.Module):
         self.obj_conv = nn.Sequential(bn(128), relu,
                                       nn.Conv2d(128, 1, 1))
 
+        # init weight and bias
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
     def forward(self, x, var=None):
         x = self.conv1(x)
         x = self.block1(x)
