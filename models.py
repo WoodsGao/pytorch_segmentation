55fc148cdcbc1048adf9fbffceb8ebbba8ad90b8
woodsgao <woodsgao@outlook.com>
2019-11-04 12:55:22 +0800

    update

--- models.py
+++ models.py
@@ -25,11 +25,7 @@ class DeepLabV3Plus(nn.Module):
             ResBlock(256, 256, dilation=12),
             ResBlock(256, 256),
             ResBlock(256, 256, dilation=18),
-            ResBlock(256, 256),
-        )
-        self.block4 = nn.Sequential(
-            ResBlock(256, 512, stride=2),
-            ResBlock(512, 512),
+            ResBlock(256, 512),
             ResBlock(512, 512, dilation=6),
             ResBlock(512, 512),
             ResBlock(512, 512, dilation=12),
@@ -40,27 +36,13 @@ class DeepLabV3Plus(nn.Module):
             ResBlock(512, 512),
         )
 
-        self.high_level_block = nn.Sequential(
-            ResBlock(512, 512),
-            ResBlock(512, 512, dilation=6),
-            ResBlock(512, 512),
+        self.aspp = Aspp(512, 256)
+        self.low_conv = BLD(128, 256)
+        self.cls_conv = nn.Sequential(
+            bn(512),
+            lrelu,
+            nn.Conv2d(512, num_classes, 3, 1, 1),
         )
-        self.high2middle = nn.Sequential(ResBlock(512, 256), )
-        self.middle_level_block = nn.Sequential(
-            ResBlock(256, 256),
-            ResBlock(256, 256, dilation=6),
-            ResBlock(256, 256),
-        )
-        self.middle2low = nn.Sequential(ResBlock(256, 128), )
-        self.low_level_block = nn.Sequential(
-            ResBlock(128, 128),
-            ResBlock(128, 128, dilation=6),
-            ResBlock(128, 128),
-        )
-
-        self.mask_conv = nn.Sequential(bn(128), lrelu,
-                                      nn.Conv2d(128, num_classes, 1))
-
         # init weight and bias
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -74,32 +56,20 @@ class DeepLabV3Plus(nn.Module):
         x = self.conv1(x)
         x = self.block1(x)
         x = self.block2(x)
-        low_level_feat = x
+        low_level_feat = self.low_conv(x)
         x = self.block3(x)
-        middle_level_feat = x
-        x = self.block4(x)
-        high_level_feat = x
-        high_level_feat = self.high_level_block(high_level_feat)
-        high_level_feat = self.high2middle(high_level_feat)
-        high_level_feat = F.interpolate(high_level_feat,
-                                        scale_factor=2,
-                                        mode='bilinear',
-                                        align_corners=True)
-        middle_level_feat = middle_level_feat + high_level_feat
-        middle_level_feat = self.middle_level_block(middle_level_feat)
-        middle_level_feat = self.middle2low(middle_level_feat)
-        middle_level_feat = F.interpolate(middle_level_feat,
-                                          scale_factor=2,
-                                          mode='bilinear',
-                                          align_corners=True)
-        low_level_feat = low_level_feat + middle_level_feat
-        low_level_feat = self.low_level_block(low_level_feat)
-        mask = self.mask_conv(low_level_feat)
-        mask = F.interpolate(mask,
-                                 scale_factor=4,
-                                 mode='bilinear',
-                                 align_corners=True)
-        return mask
+        x = self.aspp(x)
+        x = F.interpolate(x,
+                          scale_factor=2,
+                          mode='bilinear',
+                          align_corners=True)
+        x = torch.cat([x, low_level_feat], 1)
+        x = self.cls_conv(x)
+        x = F.interpolate(x,
+                          scale_factor=4,
+                          mode='bilinear',
+                          align_corners=True)
+        return x
 
 
 if __name__ == "__main__":
