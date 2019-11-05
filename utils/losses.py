5572fe234bf5fa44ec22872b3b1266e7933a432d
woodsgao <woodsgao@outlook.com>
2019-11-05 14:11:20 +0800

    normalize loss

--- utils/losses.py
+++ utils/losses.py
@@ -31,7 +31,7 @@ BCE = nn.BCEWithLogitsLoss(reduction='none')
 
 def compute_loss(outputs, targets):
     cls_loss = CE(outputs, targets)
-    cls_loss = torch.pow(cls_loss, 2)
+    cls_loss = torch.pow(cls_loss, 2) / cls_loss.max()
     cls_loss = cls_loss.mean()
     loss = cls_loss
     return loss
