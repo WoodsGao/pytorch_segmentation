81555166550fedf917568ee033f5298411a2d578
woodsgao <woodsgao@outlook.com>
2019-11-19 16:34:48 +0800

    update

--- models.py
+++ models.py
@@ -2,27 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.modules.nn import Aspp, AsppPooling, Swish, CNS, DropConnect
-from utils.modules.backbones import DenseNet, ResNet, EfficientNetB2, EfficientNetB4
+from utils.modules.backbones import BasicModel, DenseNet, ResNet, EfficientNetB2, EfficientNetB4
 import math
 
 
-class DeepLabV3Plus(nn.Module):
+class DeepLabV3Plus(BasicModel):
     def __init__(self, num_classes):
         super(DeepLabV3Plus, self).__init__()
         self.backbone = EfficientNetB2(16)
         self.aspp = Aspp(352, 128, [6, 12, 18])
         self.low_conv = CNS(24, 48, 3)
         self.cls_conv = nn.Sequential(
-            nn.Conv2d(128 + 48, num_classes, 3, padding=1)
-        )
+            nn.Conv2d(128 + 48, num_classes, 3, padding=1))
         # init weight and bias
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.GroupNorm):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+        self.init()
 
     def forward(self, x):
         x = self.backbone.block1(x)
@@ -45,7 +38,7 @@ class DeepLabV3Plus(nn.Module):
         return x
 
 
-class UNet(nn.Module):
+class UNet(BasicModel):
     def __init__(self, num_classes):
         super(UNet, self).__init__()
         self.backbone = EfficientNetB2(16)
@@ -53,13 +46,7 @@ class UNet(nn.Module):
         self.up_conv2 = CNS(96 + 48, 48)
         self.cls_conv = nn.Conv2d(48 + 24, num_classes, 1)
         # init weight and bias
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+        self.init()
 
     def forward(self, x):
         x = self.backbone.block1(x)
@@ -91,6 +78,7 @@ class UNet(nn.Module):
 
 if __name__ == "__main__":
     a = torch.ones([2, 3, 224, 224])
-    o = DeepLabV3Plus(30)(a)
+    model = DeepLabV3Plus(30)
+    o = model(a)
     print(o.shape)
     o.mean().backward()
