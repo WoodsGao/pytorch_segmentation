8d555874fd4c16b9401e2c29f54899fe6657e25a
woodsgao <woodsgao@outlook.com>
2019-12-10 14:55:53 +0800

    use a dw aspp

--- utils/modules/nn/aspp.py
+++ utils/modules/nn/aspp.py
@@ -5,10 +5,9 @@ from . import CNS, SeparableCNS
 
 
 class AsppPooling(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self):
         super(AsppPooling, self).__init__()
-        self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                                 CNS(in_channels, out_channels, 1))
+        self.gap = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x):
         size = x.size()[2:]
@@ -20,24 +19,21 @@ class AsppPooling(nn.Module):
 class Aspp(nn.Module):
     def __init__(self, in_channels, out_channels, atrous_rates, eps=1e-4):
         super(Aspp, self).__init__()
-        blocks = []
-        blocks.append(CNS(in_channels, out_channels, 1))
+        blocks = [AsppPooling()]
         for rate in atrous_rates:
             blocks.append(
-                SeparableCNS(in_channels, out_channels, dilation=rate))
-        blocks.append(AsppPooling(in_channels, out_channels))
+                CNS(in_channels, in_channels, groups=in_channels, dilation=rate))
         self.blocks = nn.ModuleList(blocks)
         self.aspp_weights = nn.Parameter(torch.ones(len(atrous_rates) + 2))
+        self.project = CNS(in_channels, out_channels, 1)
         self.eps = eps
 
     def forward(self, x):
         aspp_weights = self.aspp_weights.relu()
         aspp_weights = aspp_weights / (aspp_weights.sum(0, keepdim=True) +
                                        self.eps)
-        output = None
+        output = x * aspp_weights[-1]
         for bi, block in enumerate(self.blocks):
-            if output is None:
-                output = block(x) * aspp_weights[bi]
-            else:
-                output += block(x) * aspp_weights[bi]
+            output += block(x) * aspp_weights[bi]
+        output = self.project(output)
         return output
