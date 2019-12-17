4ba72db8ad47d3444e08615363badd45180bc642
woodsgao <woodsgao@outlook.com>
2019-12-17 16:31:05 +0800

    add segment

--- utils/segment.py
+++ utils/segment.py
@@ -7,11 +7,14 @@ from pytorch_modules.utils import device
 @torch.no_grad()
 def segment(model, img, img_size=(64, 64)):
     im0 = img
+    h, w, c = img.shape
     img = im0.copy()
     img = cv2.resize(img, img_size)
     img = img[:, :, ::-1]
     img = img.transpose(2, 0, 1)
     img = np.float32([img]) / 255.
     img = torch.FloatTensor(img).to(device)
-    pred = model(img)[0]
-    return pred.max(0)
+    pred = model(img)[0].softmax(1).cpu().numpy().transpose(1, 2, 0)
+    pred = cv2.resize(pred, (w, h))
+
+    return pred.max(2), pred.argmax(2)
